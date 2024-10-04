4f6a4b00a43ddf4dee855f8a2edb6928c51f23d5
Bavimurugan <bavimurugan23@gmail.com>
2024-10-04 08:27:25 +0530

    Added data-types.js

---
+++ data-types.js
@@ -0,0 +1,32 @@
+//string
+
+let fruit = 'lion';
+console.log(Animal)
+
+//number
+
+// integer value
+let integer_number = -3;
+console.log(integer_number);
+
+// floating-point value
+let float_number = 3.15;
+console.log(float_number);
+
+//boolean
+
+let dataChecked = true;
+console.log(dataChecked);  // true
+
+let valueCounted = false;
+console.log(valueCounted);  // false
+
+//undefined
+
+let name;
+console.log(name);  // undefined
+
+//null
+
+let number = null;
+console.log(number);  // null
