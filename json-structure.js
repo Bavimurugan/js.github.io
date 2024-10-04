36b6a23446b07b172956ead1f868c42c813bfaf1
Bavimurugan <bavimurugan23@gmail.com>
2024-10-04 08:55:38 +0530

    Added json-structure.js

---
+++ json-structure.js
@@ -0,0 +1,17 @@
+//json structure
+{
+    "name": "pavi",
+    "age":23 ,
+    "isStudent": false,
+    "courses": [
+      "Mathematics",
+      "Science",
+      "History"
+    ],
+    "address": {
+      "street": "123 Main St",
+      "city": "chennai",
+      "zipCode": "600123"
+    }
+  }
+  
